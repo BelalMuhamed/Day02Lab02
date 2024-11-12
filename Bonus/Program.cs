9e852b2239f996c07dfa53085e8db2782b8973e1
BelalBasal <basdibrahim@gmail.com>
2024-11-12 22:00:07 +0200

    reversed array

--- Bonus/Program.cs
+++ Bonus/Program.cs
@@ -2,6 +2,8 @@
 {
     public class Program
     {
+        //create array 
+        #region Create Array 
         public static int[] CreateArray() 
         {
             int size;
@@ -26,6 +28,8 @@
             }
             return NumbersArray;
         }
+        #endregion
+        ////max length
         #region Get Max Int
         public static int GetMaxNumberOfTheArray(int[] Array)
         {
@@ -40,12 +44,43 @@
             return MaxNumber;
         }
         #endregion
+        //swap two numbers 
+        #region Swap
+        public static void Swap(ref int num01,ref int num02)
+        {
+            int temp = num02;
+            num02 = num01;
+            num01 = temp;
+        }
+        #endregion
+        //reverse array
+        #region Reverse Elements of the array
+        public static void ReverseArray(int[] array)
+        {
+            if (array.Length > 1)
+            {
+                for (int i = 0; i < array.Length / 2; i++)
+                {
+                    int k = array.Length - i - 1;
+                    Swap(ref array[i],ref array[k]);
+                }
+            }
+        }
+        #endregion
         static void Main(string[] args)
         {
+            //create array 
             int[] ArrayOfNumbers = CreateArray();
+            //max length
             int MaxOfTheArray = GetMaxNumberOfTheArray(ArrayOfNumbers);
             Console.WriteLine($"MAx Number in this array is : {MaxOfTheArray}");
-
+            //reversed array 
+            ReverseArray(ArrayOfNumbers);
+            Console.WriteLine("Reversed Array  : ");
+            for (int i = 0; i < MaxOfTheArray; i++) 
+            {
+                Console.WriteLine($"Element {i+1} : {ArrayOfNumbers[i]}");
+            }
 
         }
     }
