0f9bbeacb13fd5a2156a3af918524d645fe28a21
BelalBasal <basdibrahim@gmail.com>
2024-11-13 00:11:19 +0200

    02

---
+++ BonusPart02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BonusPart02
+{
+    public class Program
+    {
+        public static long GetCountOfOnes()
+        {
+            long counter =0;
+            for (long i = 1; i <= 99999999;i++)
+            {
+                long n = i;
+                while(n>0)
+                {
+                    int digit =(int)( n % 10);
+                    
+                    if(digit ==1)
+                    {
+                        counter++;
+                    }
+                    n /= 10;
+                }
+            }
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            long numberof1s = GetCountOfOnes();
+            Console.WriteLine(numberof1s);
+            String s = "99999999";
+            Console.WriteLine(s.Length * Math.Pow(10, s.Length - 1));
+
+        }
+    }
+}
